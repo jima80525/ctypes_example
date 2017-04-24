255d8967de373e9efcbe3252256bcad1a78d66e0
Jim Anderson <jima80525@gmail.com>
2017-04-24 07:07:36 -0600

    added string examples and started malloc

--- clib1/callClib1.py
+++ clib1/callClib1.py
@@ -7,3 +7,18 @@ print(libc.func1_no_args())
 print(libc.func1_no_args())
 print(libc.func1_no_args())
 print(libc.func1_no_args())
+
+# python strings are immutable.
+x = "staring string"
+print(x)
+libc.func2_string_add_one(x)  # should fail
+print(x)
+
+y = create_string_buffer(str.encode(x)) # need to encode this to get bytes
+print(y.value)
+libc.func2_string_add_one(y)  # should fail
+print(y.value)
+
+libc.func3_return_string.restype = c_char_p  # c_char_p is a pointer to a string
+where = libc.func3_return_string()
+print(where)
--- clib1/clib1.c
+++ clib1/clib1.c
@@ -1,6 +1,19 @@
+#include <string.h>
 
 int func1_no_args(void) {
     static int counter = 0;
     counter++;
     return counter;
+}
+
+void func2_string_add_one(char *input) {
+    int ii = 0;
+    for (; ii < strlen(input); ii++) {
+        input[ii]++;
+    }
+}
+
+char * func3_return_string(void) {
+    char* name = strdup("I was written in C");
+    return name;
 }
